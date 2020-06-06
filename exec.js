1851e06e5b93a6c1f2ee9536a97bb47c3ed421a5
RediPanda <redipanda@loli.airforce>
2020-06-06 12:17:27 +1000

    Origin push

---
+++ exec.js
@@ -0,0 +1,105 @@
+const {
+  app,
+  BrowserWindow,
+  ipcMain
+} = require('electron')
+
+require('v8-compile-cache')
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 1300,
+    height: 700,
+    frame: false,
+    ELECTRON_DISABLE_SECURITY_WARNINGS: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+
+function getData() {
+  console.log('Yay!')
+  alert('An error has occured!')
+}
+
+function connectSession() {
+
+}
+
+function launchGame() {
+
+}
+
+function databaseQuery(args, query) {
+  // perf fix
+  // const pool = require('mysql2');
+
+}
+
+function downloadPipe(url, dest, name) {
+  /**
+   * @params {string} The url of the origin source.
+   * @params {string} The destination folder.
+   * @params {string} The branding name.
+   */
+}
+
+// Event listeners from the IPC Pipeline.
+
+// Attach listener in the main process with the given ID
+ipcMain.on('request-mainprocess-action', (event, arg) => {
+  // Displays the object sent from the renderer process:
+  //{
+  //    message: "Hi",
+  //    someData: "Let's go"
+  //}
+
+
+    // Download module.
+
+    const fs = require('fs');
+    const download = require('download');
+
+    (async () => {
+      // await download('https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg', 'dist');
+   
+      fs.writeFileSync('dist/test.jpg', await download('https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg'));
+   
+      // download('https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg').pipe(fs.createWriteStream('dist/test.jpg'));
+    })();
+
+});
---
+++ src/karina-lib/sessionManager.js
@@ -0,0 +1,233 @@
+// External libraries
+const fetch = require('node-fetch');
+const fs = require('fs');
+const random = require('randomatic');
+const Gamedig = require('gamedig');
+const extract = require('extract-zip');
+const mysql = require('mysql2');
+
+// Internal libraries
+const atsume = require('./src/karina-lib/atsumeLib.js');
+const runtime = require('./src/karina-lib/runtime.js');
+
+// Internal variables.
+const localfileVersion = 'v1.0.2';
+
+// Establish database connections.
+// SIKE, FUCK DATABASES.
+
+
+
+// Token clearance
+let finalToken = `${random('Aa0', 5)}-${random('Aa0', 5)}-${random('Aa0', 5)}-${random('Aa0', 5)}-${random('Aa0', 5)}`;
+atsume.logger(`INFO`, `Registering program session as (${finalToken})`);
+
+// On page load/app load, authenticate the user using old data.
+async function serviceAuthentication() {
+    atsume.logger(`INFO`, `[Session Manager]: Checking Mojang Authentication Servers...`);
+
+    const panel = document.getElementById('accountPanel');
+    const text = document.getElementById('panelText');
+    const status = document.getElementById('panelStatus');
+
+    atsume.logger(`INFO`, "[Session Manager @ fs]: Grabbing authentication packets...")
+
+    let inputEmail;
+    let inputPass;
+
+    try {
+        if (fs.existsSync('src/profile')) {
+            atsume.logger(`INFO`, "[Session Manager @ fs]: Profiler is present during load. Loading information to the module.")
+
+            let authDataRaw = await fs.readFileSync('src/profile/auth.json', 'utf8')
+            let authData = JSON.parse(authDataRaw)
+
+            if (authData.email === undefined) {
+                if (confirm("[Velox Security Wall] Missing auth details (@username)")) {
+                    window.close()
+                } else {
+                    window.close()
+                }
+            } else if (authData.password === undefined) {
+                if (confirm("[Velox Security Wall] Missing auth details (@password)")) {
+                    window.close()
+                } else {
+                    window.close()
+                }
+            }
+            inputEmail = authData.email
+            inputPass = authData.password
+        }
+    } catch (err) {
+        alert("Failed to find the profiling directory!")
+        return atsume.logger(`ERROR`, err)
+    }
+
+    let body = {
+        "agent": { // defaults to Minecraft
+            "name": "Minecraft", // For Mojang's other game Scrolls, "Scrolls" should be used
+            "version": 1 // This number might be increased
+            // by the vanilla client in the future
+        },
+        "username": inputEmail, // Can be an email address or player name for
+        // unmigrated accounts
+        "password": inputPass,
+        "requestUser": true // optional; default: false; true adds the user object to the response
+    }
+
+    let obj;
+
+    atsume.logger(`API`, `[Session Manager]: Sending POST request to authserver.mojang.com/authenticate`);
+    atsume.logger(`API`, `[Session Manager]: Authenticating user under the identity - (${inputEmail})`)
+
+    await fetch('https://authserver.mojang.com/authenticate', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(res => res.json())
+        .then(json => {
+            // console.log(json)
+            obj = json
+        });
+
+        atsume.logger(`API`, `[Session Manager]: Received callback response.`);
+
+    if (obj.error === "ForbiddenOperationException") {
+        atsume.logger(`API`, `[Session Manager @ Authentication]: Failed to authenticate user @${inputEmail}`)
+        text.innerHTML = 'Failed to authenticate your login credentials!';
+        fs.writeFile('cache/session-lock.json', JSON.stringify({
+            token: finalToken,
+            date: Date.now(),
+            boolean: 0
+        }), function (err, results) {
+            // console.log(err + '  ' + results) 
+        });
+    } else if (obj.accessToken) {
+        atsume.logger(`API`, `[Session Manager @ Authentication]: Successfully authenticated user @${inputEmail}`)
+        // text.innerHTML = 'Successfully validated your session!';
+        fs.writeFile('cache/session-lock.json', JSON.stringify({
+            token: finalToken,
+            date: Date.now(),
+            boolean: 1
+        }), function (err, results) {
+            // console.log(err + '  ' + results)
+        });
+    } else {
+        return text.innerHTML = 'An internal error has occured. Please restart the application or contact the developer with the appropriate log files.';
+    }
+
+};
+
+// On page load/app load, open event listeners. (Button Functions)
+async function buttonListeners() {
+    atsume.logger(`INFO`, `[Session Manager]: Listening for Play Triggers.`);
+
+    document.getElementById("playBtn1").addEventListener("click", () => {
+        alert('Launching the game. Please wait!')
+        runtime.runApplication('ipconfig');
+        getUpdate()
+    });
+
+    document.getElementById("playBtn2").addEventListener("click", () => {
+        alert('Launching the game. Please wait!')
+        runtime.runApplication('ipconfig');
+    });
+
+    document.getElementById("updateBtn").addEventListener("click", () => {
+        getUpdate();
+    });
+
+    async function applyUpdate() {
+        atsume.logger(`DATABASE`, `[Session Manager @ applyUpdate]: Callback received.`);
+        alert("Update applying.");
+    };
+
+    async function getUpdate() {
+        atsume.logger(`DATABASE`, `[Session Manager @ getUpdate]: Callback received.`);
+        alert("Update getting. Please wait as we are checking the current version number.");
+
+        let versionBuffer = fs.readFileSync('src/instance/redis.json')
+        let latestVersion = fetch()
+        if (versionBuffer) {}
+    };
+
+    // Once the DOM Content load, we shall check for any latest updates.
+    try {
+        // Read off existing profile settings.
+        if (fs.existsSync('src/profile')) {
+            atsume.logger(`INFO`, `[Session Manager @ apt-module]: Profile directory detected.`);
+        }
+    } catch(err) {
+        // Compile new profile settings based on SessionManager version.
+        fs.writeFile('src/profile/launcher-settings.json', JSON.stringify({
+            version: localfileVersion
+        }), function (err, results) {
+            // console.log(err + '  ' + results)
+        });
+    };
+};
+
+// On page load/app load, perform server status check.
+async function serverQuery() {
+    atsume.logger(`INFO`, `[Session Manager]: Starting Server Query...`);
+
+    setInterval(async function getServerQuery() {
+        atsume.logger(`API`, `[Session Manager @ Query]: Starting Heartbeat roundtrip...`);
+        // Obtain local storage files. If not, generate new ones.
+        let contentData = fs.readFileSync('src/karina-lib/patch.json', 'utf8');
+        let objectData = JSON.parse(contentData)
+
+        let btnElement = document.getElementById('statusBar')
+
+        let status_boolean = 0;
+
+        await Gamedig.query({
+            type: 'minecraft',
+            host: objectData.serverBearer,
+            maxAttempts: 3
+        }).then((state) => {
+            atsume.logger(`API`, '[Server Query] Stacktrace Online ')
+            status_boolean = 1;
+        }).catch((error) => {
+            atsume.logger(`API`, '[Server Query] Stacktrace Offline ')
+            status_boolean = 0;
+        });
+
+        if (status_boolean == '1') {
+            // Server's online!
+            btnElement.classList.remove('badge-primary')
+            btnElement.classList.remove('badge-danger')
+            btnElement.classList.add('badge-success')
+            btnElement.innerHTML = "Online"
+        } else {
+            // Assume the server's unreachable.
+            btnElement.classList.remove('badge-primary')
+            btnElement.classList.remove('badge-success')
+            btnElement.classList.add('badge-danger')
+            btnElement.innerHTML = "Offline"
+        }
+    }, 7000)
+
+};
+
+// Setup check and apply.
+async function Setup() {
+    if (!fs.existsSync('src/profile/auth.json')) {
+        fs.writeFileSync('src/profile/auth.json', JSON.stringify({
+            email: 'undefined',
+            password: 'undefined'
+        }));
+    }
+}
+
+// On page load/app load, perform setup checks.
+document.addEventListener('DOMContentLoaded', async function() {
+    atsume.logger(`INFO`, `[Session Manager]: DOMContent Loaded! Starting other function services...`);
+
+    serverQuery();
+    serviceAuthentication();
+    buttonListeners();
+
+});
