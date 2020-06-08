aa2b1e208fd85a517bb35b810328d8185ede8395
RediPanda <redipanda@loli.airforce>
2020-06-08 15:54:32 +1000

    Update frontEndHandler.js

--- src/karina-lib/frontEndHandler.js
+++ src/karina-lib/frontEndHandler.js
@@ -23,7 +23,7 @@ async function buttonListeners() {
 
         // Checking for any updates pending.
         let currentVersion;
-        updateLoadBar(true, '10%', 'Checking for updates...');
+        updateLoadBar(true, '10', 'Checking for updates...');
         if (!fs.existsSync(`${process.env.APPDATA}//devpanda//client//profile//modvar.json`)) {
             fs.writeFileSync(`${process.env.APPDATA}//devpanda//client//profile//modvar.json`, '1.0.0')
             currentVersion = '1.0.0'
@@ -31,14 +31,14 @@ async function buttonListeners() {
             currentVersion = await fs.readFileSync(`${process.env.APPDATA}//devpanda//client//profile//modvar.json`);
         }
 
-        updateLoadBar(true, '30%', 'Acquiring modpack information...');
+        updateLoadBar(true, '30', 'Acquiring modpack information...');
 
         let getJSON;
         await fetch('http://www.veloxnetwork.ml/database/db.json')
             .then(res => res.json())
             .then(json => getJSON = json);
 
-        updateLoadBar(true, '60%', 'Checking for updates...');
+        updateLoadBar(true, '60', 'Checking for updates...');
 
         let latest = getJSON.launcher[2].latestModpackVersion;
 
@@ -55,21 +55,21 @@ async function buttonListeners() {
 
                 // Libraries
                 atsume.logger(`INFO`, `Importing package [Libraries]`);
-                updateLoadBar(true, '0%', `Downloading Patch (libraries) from the Resource Manager...`);
+                updateLoadBar(true, '0', `Downloading Patch (libraries) from the Resource Manager...`);
                 fs.writeFileSync(`${process.env.APPDATA}\\devpanda\\cache\\libraries.zip`,
                     await download(`http://www.veloxnetwork.ml/files/essentials/v${latest}/libraries.zip`).on('downloadProgress', (callback) => {
-                        updateLoadBar(true, `${Math.round(callback.percent*100)}%`, `Downloading Patch (libraries) from the Resource Manager...`);
+                        updateLoadBar(true, `${Math.round(callback.percent*100)}`, `Downloading Patch (libraries) from the Resource Manager...`);
                     }));
                 setTimeout(function finishCallback() {
-                    updateLoadBar(true, '100%', `Resolving data from Patch (libraries) from the Resource Manager...`);
+                    updateLoadBar(true, '100', `Resolving data from Patch (libraries) from the Resource Manager...`);
                 }, 950);
 
                 // Libraries
                 atsume.logger(`INFO`, `Importing package [Assets]`);
-                updateLoadBar(true, '0%', `Downloading Patch (assets) from the Resource Manager...`);
+                updateLoadBar(true, '0', `Downloading Patch (assets) from the Resource Manager...`);
                 fs.writeFileSync(`${process.env.APPDATA}\\devpanda\\cache\\assets.zip`,
                     await download(`http://www.veloxnetwork.ml/files/essentials/v${latest}/assets.zip`).on('downloadProgress', (callback) => {
-                        updateLoadBar(true, `${Math.round(callback.percent*100)}%`, `Downloading Patch (assets) from the Resource Manager...`);
+                        updateLoadBar(true, `${Math.round(callback.percent*100)}`, `Downloading Patch (assets) from the Resource Manager...`);
                     }));
                 setTimeout(function finishCallback() {
                     updateLoadBar(true, '100%', `Resolving data from Patch (assets) from the Resource Manager...`);
@@ -78,35 +78,35 @@ async function buttonListeners() {
                 // Download all mod-related packages.
                 for (i = 0; i < array.length; i++) {
                     atsume.logger(`INFO`, `Importing package ${i + 1}`);
-                    updateLoadBar(true, '0%', `Downloading Patch ${i + 1} from the Resource Manager...`);
+                    updateLoadBar(true, '0', `Downloading Patch ${i + 1} from the Resource Manager...`);
                     fs.writeFileSync(`${process.env.APPDATA}\\devpanda\\cache\\pack${i + 1}.zip`,
                         await download(`http://www.veloxnetwork.ml/files/mods/v${latest}/pack${i + 1}.zip`).on('downloadProgress', (callback) => {
-                            updateLoadBar(true, `${Math.round(callback.percent*100)}%`, `Downloading Patch ${i + 1} from the Resource Manager...`);
+                            updateLoadBar(true, `${Math.round(callback.percent*100)}`, `Downloading Patch ${i + 1} from the Resource Manager...`);
                         }));
                     setTimeout(function finishCallback() {
-                        updateLoadBar(true, '100%', `Resolving data from Patch ${i + 1} from the Resource Manager...`);
+                        updateLoadBar(true, '100', `Resolving data from Patch ${i + 1} from the Resource Manager...`);
                     }, 950);
                 }
 
                 // Check and download instance package.
                 if (!fs.existsSync(`${process.env.APPDATA}\\devpanda\\instance\\bin\\minecraft.jar`)) {
                     atsume.logger(`Importing package [MAIN INSTANCE PACKAGE]`);
-                    updateLoadBar(true, '0%', `Downloading instance.zip from the Resource Manager...`);
+                    updateLoadBar(true, '0', `Downloading instance.zip from the Resource Manager...`);
                     fs.writeFileSync(`${process.env.APPDATA}\\devpanda\\cache\\instance.zip`,
                         await download(`http://www.veloxnetwork.ml/files/instance/v${latest}/instance.zip`).on('downloadProgress', (callback) => {
-                            updateLoadBar(true, `${Math.round(callback.percent*100)}%`, `Downloading instance.zip from the Resource Manager...`);
+                            updateLoadBar(true, `${Math.round(callback.percent*100)}`, `Downloading instance.zip from the Resource Manager...`);
                         }));
                     setTimeout(function finishCallback() {
-                        updateLoadBar(true, '100%', `Resolving data from instance.zip from the Resource Manager...`);
+                        updateLoadBar(true, '100', `Resolving data from instance.zip from the Resource Manager...`);
                     }, 950);
 
                     // Now unpackage and install instance.
                     atsume.logger(`INFO`, `Extracting the instance.zip file.`)
-                    updateLoadBar(true, '1%', `Extracting packet [instance.zip]`);
+                    updateLoadBar(true, '1', `Extracting packet [instance.zip]`);
                     await extract(`${process.env.APPDATA}\\devpanda\\cache\\instance.zip`, {
                         dir: `${process.env.APPDATA}\\devpanda\\instance`
                     });
-                    updateLoadBar(true, '100%', `Extracting packet [instance.zip]`);
+                    updateLoadBar(true, '100', `Extracting packet [instance.zip]`);
                 }
 
                 // Clear and re-apply mods to the instance folder.
@@ -119,33 +119,33 @@ async function buttonListeners() {
                 }
 
                 atsume.logger(`INFO`, `Extracting individual mod packages.`)
-                updateLoadBar(true, '1%', `Extracting packet [Patch ${i + 1}]`);
+                updateLoadBar(true, '1', `Extracting packet [Patch ${i + 1}]`);
                 for (i = 0; i < array.length; i++) {
                     await extract(`${process.env.APPDATA}\\devpanda\\cache\\pack${i + 1}.zip`, {
                         dir: `${process.env.APPDATA}\\devpanda\\instance\\mods`
                     });
-                    updateLoadBar(true, '100%', `Extracting packet [Patch ${i + 1}]`);
+                    updateLoadBar(true, '100', `Extracting packet [Patch ${i + 1}]`);
                 }
 
                 atsume.logger(`INFO`, `Extracting assets.`)
-                updateLoadBar(true, '1%', `Extracting packet [assets]`);
+                updateLoadBar(true, '1', `Extracting packet [assets]`);
                 for (i = 0; i < array.length; i++) {
                     await extract(`${process.env.APPDATA}\\devpanda\\cache\\assets.zip`, {
                         dir: `${process.env.APPDATA}\\devpanda\\assets`
                     });
-                    updateLoadBar(true, '100%', `Extracting packet [assets]`);
+                    updateLoadBar(true, '100', `Extracting packet [assets]`);
                 }
 
                 atsume.logger(`INFO`, `Extracting libraries`)
-                updateLoadBar(true, '1%', `Extracting packet [libraries]`);
+                updateLoadBar(true, '1', `Extracting packet [libraries]`);
                 for (i = 0; i < array.length; i++) {
                     await extract(`${process.env.APPDATA}\\devpanda\\cache\\libraries.zip`, {
                         dir: `${process.env.APPDATA}\\devpanda\\libraries`
                     });
-                    updateLoadBar(true, '100%', `Extracting packet [libraries]`);
+                    updateLoadBar(true, '100', `Extracting packet [libraries]`);
                 }
 
-                updateLoadBar(false, '0%', 'Done!');
+                updateLoadBar(false, '0', 'Done!');
 
                 // And finally, apply patch update to the version medium.
                 fs.writeFileSync(`${process.env.APPDATA}//devpanda//client//profile//modvar.json`, latest);
@@ -157,9 +157,9 @@ async function buttonListeners() {
 
         await getSetUpdate();
         setTimeout(function startGame() {
-            updateLoadBar(true, '100%', 'Running Java arguments...');
+            updateLoadBar(true, '100', 'Running Java arguments...');
             setTimeout(async function startGame() {
-                updateLoadBar(false, '0%', '.');
+                updateLoadBar(false, '0', '.');
                 let args = await runtime.createArguments();
                 runtime.runApplication(args);
             }, 750)
@@ -276,8 +276,8 @@ function updateLoadBar(boolean, percentage, text) {
     }
 
     // Set percentage.
-    syncbar.style.width = percentage;
-    $('#download-progress-percent').innerHTML = percentage;
+    syncbar.style.width = `${percentage}%`;
+    $('#download-progress-percent').innerHTML = `${Math.round(percentage)}%`;
 
     // Set text.
     textbar.innerHTML = text;
